5fab7c0f222849c0504556c650c83bba62977ae9
asahi <hx233@live.com>
2019-08-31 16:29:45 +0800

    Site updated: 2019-08-31 16:29:45

--- dist/music.js
+++ dist/music.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-30 21:21:43
- * @LastEditTime: 2019-08-31 16:07:32
+ * @LastEditTime: 2019-08-31 16:29:09
  * @LastEditors: Please set LastEditors
  */
 const ap = new APlayer({
@@ -17,7 +17,7 @@ const ap = new APlayer({
         name: "bgmusic",
         artist: 'other',
         url: 'images/llss.mp3',
-        cover: '',
+        cover: 'images/cover.png',
         lrc: '',
       },
     ]
